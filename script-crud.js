3db4c5e3eb08f9fe13441f9144e3842a37492f80
Greg Schwed <gregory.w.schwed@gmail.com>
2024-02-17 11:49:30 -0300

    permitir limpar histórico

--- script-crud.js
+++ script-crud.js
@@ -2,6 +2,8 @@
 
 //Adicionar nova tarefa
 const btnAdicionarTarefa = document.querySelector('.app__button--add-task')
+const btnLimparTarefa = document.getElementById('btn-remover-todas')
+
 const formAdicionarTarefa = document.querySelector('.app__form-add-task')
 const txtAreaAdicionarTarefa = document.querySelector('.app__form-textarea')
 const tarefas = JSON.parse(localStorage.getItem('tarefas')) || []
@@ -13,12 +15,10 @@ function listarItemTarefa (tarefa){
 
     const svg = document.createElement('svg')
     svg.innerHTML = `
-    <svg>
         <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
             <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z" fill="#01080E"></path>
         </svg>
-    </svg>
     `
 
     const paragrafo = document.createElement('p')
@@ -40,7 +40,10 @@ function listarItemTarefa (tarefa){
 }
 
 
-
+btnLimparTarefa.addEventListener('click', () =>{
+    localStorage.clear()
+    
+})
 
 btnAdicionarTarefa.addEventListener('click', () =>{
     formAdicionarTarefa.classList.toggle('hidden')
@@ -52,7 +55,11 @@ formAdicionarTarefa.addEventListener('submit', (evento) =>{
         descricao: txtAreaAdicionarTarefa.value
     }
     tarefas.push(tarefa)
+    const itemLista = listarItemTarefa(tarefa)
+    ulTarefas.append(itemLista)
     localStorage.setItem('tarefas', JSON.stringify(tarefas))
+    txtAreaAdicionarTarefa.value=''
+    formAdicionarTarefa.classList.add('hidden')
 })
 
 
